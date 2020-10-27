3ec9b66b91f92246117d3d19eb89cf08fd266b5a
Harry <harry811016@gmail.com>
2020-10-27 20:19:49 +0800

    右Navbar初版

--- src/components/RightNavbar.js
+++ src/components/RightNavbar.js
@@ -1,16 +1,19 @@
 import React from "react";
+import homePicNav from "../image/tomato.png";
+import musicPicNav from "../image/music.png";
+import statisticPicNav from "../image/chart.png";
 
 const RightNavbar = () => {
   return (
     <div className="rightNavbar-container">
-      <a className="Home-page" href="/" target="_blank">
-        1
+      <a className="Home-page nav-choose" href="/" target="_blank">
+        <img src={homePicNav} className="homePic" alt="home" />
       </a>
       <a className="Music-page" href="/" target="_blank">
-        2
+        <img src={musicPicNav} className="musicPic" alt="home" />
       </a>
       <a className="Statistic-page" href="/" target="_blank">
-        3
+        <img src={statisticPicNav} className="statisticPic" alt="home" />
       </a>
     </div>
   );
