f5630a65a0e049590e8d3543de0be756a38136ff
Harry <harry811016@gmail.com>
2020-10-28 01:02:23 +0800

    title區塊初版/整體版面微調

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import "./App.css";
 import TodoList from "./components/TodoList";
 import Counter from "./components/Counter";
 import RightNavbar from "./components/RightNavbar";
+import TitlePic from "./image/Group_39.png";
 
 function App() {
   return (
     <div className="App">
-      <div className="App_name">
-        <h2>WEEK 1</h2>
-        <h1>POMODOR</h1>
+      <div className="App_namePart">
+        <h2 className="App_namePart_1">WEEK 1</h2>
+        <div className="App_namePart_2">
+          <span>POMODOR</span>
+          <img
+            src={TitlePic}
+            className="name_titlePic"
+            alt="title pic"
+            height="100px"
+          />
+        </div>
       </div>
       <div className="App_content">
         <TodoList />
