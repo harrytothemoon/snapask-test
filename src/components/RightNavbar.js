364f752662f2435f2f690ef28b54f6fb2f07a67d
Harry <harry811016@gmail.com>
2020-10-29 14:58:33 +0800

    增加RightNavbar功能

--- src/components/RightNavbar.js
+++ src/components/RightNavbar.js
@@ -3,24 +3,44 @@ import icon_home_rightbar from "../image/tomato.png";
 import icon_music_rightbar from "../image/music.png";
 import icon_statistic_rightbar from "../image/chart.png";
 
+const { useState } = React;
+
 const RightNavbar = () => {
+  const [toggleClass, setToggleClass] = useState("home");
+  const handleClick = (type) => {
+    setToggleClass(type);
+  };
+
   return (
     <div className="rightNavbar_container">
-      <button className="Home_page nav_choose">
+      <button
+        onClick={() => handleClick("home")}
+        className={`Home_page ${toggleClass === "home" ? "nav_choose" : null}`}
+      >
         <img
           src={icon_home_rightbar}
           className="icon_home_rightbar"
           alt="home"
         />
       </button>
-      <button className="Music_page">
+      <button
+        onClick={() => handleClick("music")}
+        className={`Music_page ${
+          toggleClass === "music" ? "nav_choose" : null
+        }`}
+      >
         <img
           src={icon_music_rightbar}
           className="icon_music_rightbar"
           alt="home"
         />
       </button>
-      <button className="Statistic_page">
+      <button
+        onClick={() => handleClick("statistic")}
+        className={`Statistic_page ${
+          toggleClass === "statistic" ? "nav_choose" : null
+        }`}
+      >
         <img
           src={icon_statistic_rightbar}
           className="icon_statistic_rightbar"
